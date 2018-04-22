046b2b92edd8fedd7bf674b844fe77059812187c
nspp99b <chromepanthers@gmail.com>
2018-04-22 17:15:43 -0400

    moves logic to background.js and skim.js

---
+++ skim.js
@@ -0,0 +1,20 @@
+(function() {
+  const mark = (str) => {
+    return "<mark>" + str + "</mark>"
+  }
+
+  const skim = (p) => {
+    let s = p.innerText.match(/\(?[^\.\?\!]+[\.!\?]\)?/g)
+    s[0] = mark(s[0])
+    s[s.length-1] = mark(s[s.length-1])
+    return s.join(" ")
+  }
+
+  const d = document;
+  const article = d.getElementById('articleBody')
+  const pars = article.childNodes[0].childNodes[0].childNodes[1]
+  for (let i=0; i<pars.children.length; i++) {
+    console.log(skim(pars.children[i]))
+    pars.children[i].innerHTML = skim(pars.children[i])
+  }
+})()
