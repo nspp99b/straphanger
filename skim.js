242ef0452cd6848edc5cd9ea5e50f2d5873442bb
nspp99b <chromepanthers@gmail.com>
2018-05-04 23:53:58 -0400

    adds semicolons

--- skim.js
+++ skim.js
@@ -1,21 +1,21 @@
 (function() {
   const mark = (str) => {
-    return "<mark>" + str + "</mark>"
+    return "<mark>" + str + "</mark>";
   }
 
   const skim = (p) => {
-    let s = p.innerText.split(/(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?)\s/gm)
-    s[0] = mark(s[0])
-    s[s.length-1] = mark(s[s.length-1])
-    return s.join(" ")
+    let s = p.innerText.split(/(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?)\s/gm);
+    s[0] = mark(s[0]);
+    s[s.length-1] = mark(s[s.length-1]);
+    return s.join(" ");
   }
   const d = document;
-  const sections = d.getElementsByClassName("SectionBreak__sectionBreak___1ppA7")
+  const sections = d.getElementsByClassName("SectionBreak__sectionBreak___1ppA7");
   for (let i=1; i<sections.length; i++) {
-    let pars = sections[i]
+    let pars = sections[i];
     for (let i=0; i<pars.children.length; i++) {
       pars.children[i].innerHTML = skim(pars.children[i])
-    }
-  }
+    };
+  };
 
-})()
+})();
